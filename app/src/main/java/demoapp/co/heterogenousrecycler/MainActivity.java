f07298262531b3d8a4a541efcb36817e87b5c9ac
surya <surya.qmax@gmail.com>
2018-05-16 12:52:03 +0530

    first commit

---
+++ app/src/main/java/demoapp/co/heterogenousrecycler/MainActivity.java
@@ -0,0 +1,37 @@
+package demoapp.co.heterogenousrecycler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        ArrayList<Model> list= new ArrayList();
+        list.add(new Model(Model.TEXT_TYPE,"Hello. This is the Text-only View Type. Nice to meet you",0));
+        list.add(new Model(Model.IMAGE_TYPE,"Hi. I display a cool image too besides the omnipresent TextView.",R.drawable.ic_launcher_background));
+        list.add(new Model(Model.IMAGE_TYPE,"Hi again. Another cool image here. Which one is better?",R.drawable.ic_launcher_background));
+
+        MultiViewTypeAdapter adapter = new MultiViewTypeAdapter(list,this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, OrientationHelper.VERTICAL, false);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(adapter);
+    }
+
+
+}
---
+++ app/src/main/java/demoapp/co/heterogenousrecycler/Model.java
@@ -0,0 +1,23 @@
+package demoapp.co.heterogenousrecycler;
+
+/**
+ * Created by surya on 5/5/18.
+ */
+
+public class Model {
+
+    public static final int TEXT_TYPE=0;
+    public static final int IMAGE_TYPE=1;
+    public static final int AUDIO_TYPE=2;
+
+    public int type;
+    public int data;
+    public String text;
+
+    public Model(int type, String text, int data)
+    {
+        this.type=type;
+        this.data=data;
+        this.text=text;
+    }
+}
